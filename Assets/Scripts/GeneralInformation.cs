bbed05b67c17c034ef78f95197580997e3d7deb8
DalaiLomo <reisereise.85@gmail.com>
2021-06-24 11:58:03 +0200

    First script
    - Added first script with console logs
    - Attached script to an empty game object

---
+++ Assets/Scripts/GeneralInformation.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeneralInformation : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello from Start function");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Hello from Update function");
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Example 2D project in Unity
+
+## Caveats
+
+-   Remember that the name of the script file MUST MATCH ALWAYS the name of the class to avoid eternal pain and suffering.
